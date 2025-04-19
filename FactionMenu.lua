263bc10e84b3fe3b3cbc319172fb0690e46d5aac
Gazmik Fizzwidget <gazmik@fizzwidget.com>
2025-04-19 14:43:28 -0700

    show standing in rep watch bar menu

--- FactionMenu.lua
+++ FactionMenu.lua
@@ -4,6 +4,40 @@ local addonName, T = ...
 ------------------------------------------------------
 -- Utilities
 ------------------------------------------------------
+
+function T:StandingText(factionData)
+    -- local factionData = C_Reputation.GetFactionDataByID(factionID)
+    local factionID = factionData.factionID
+    local friendshipData = C_GossipInfo.GetFriendshipReputation(factionID)
+    local isFriendship = friendshipData and friendshipData.friendshipFactionID > 0
+        
+    -- if C_Reputation.IsAccountWideReputation(factionID) then
+    --     -- TODO something in menu for this?
+    -- end
+        
+    if isFriendship then
+        local reactionText = friendshipData.reaction
+        -- friendship is always green 
+        -- because don't know how many levels above / below
+        local color = FACTION_BAR_COLORS[5]
+        return reactionText, color
+        
+    elseif C_Reputation.IsMajorFaction(factionID) then
+        local majorFactionData = C_MajorFactions.GetMajorFactionData(factionID)
+                
+        local renownLevel = RENOWN_LEVEL_LABEL:format(majorFactionData.renownLevel)
+                    
+        return renownLevel, BLUE_FONT_COLOR
+
+    else		
+        local standingText = GetText("FACTION_STANDING_LABEL"..factionData.reaction, UnitSex("player"))
+        local color = FACTION_BAR_COLORS[factionData.reaction]
+                
+        return standingText, color
+    end
+    
+end
+
 function T.ShowFactionMenu(frame)
     MenuUtil.CreateContextMenu(frame, function(owner, root)
         
@@ -17,7 +51,17 @@ function T.ShowFactionMenu(frame)
         
         for index, factionID in pairs(T.Recents) do
             local factionData = C_Reputation.GetFactionDataByID(factionID)
-            root:CreateRadio(factionData.name, T.MenuFactionButtonIsChecked, T.MenuFactionButtonSetChecked, factionID)
+            local radio = root:CreateRadio(factionData.name, T.MenuFactionButtonIsChecked, T.MenuFactionButtonSetChecked, factionID)
+            radio:AddInitializer(function(frame, description, menu)
+                local fontString2 = frame:AttachFontString()
+                fontString2:SetHeight(20)
+                fontString2:SetPoint("RIGHT")
+
+                local text, color = T:StandingText(factionData)
+                fontString2:SetTextToFit(text)
+                fontString2:SetTextColor(color:GetRGBA())
+            end)
+
         end
         
         root:CreateDivider()
